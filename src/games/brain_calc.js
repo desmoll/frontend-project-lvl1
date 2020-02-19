58b21b8ff18989890e6a35a19cf6f55300d09ced
desmoll <desmollx@gmail.com>
2020-02-19 11:12:21 +0300

    code refactoring

--- src/games/brain_calc.js
+++ src/games/brain_calc.js
@@ -2,7 +2,7 @@ import getRandomInt from '../utils.js';
 import run from '../index.js';
 
 const task = 'What is the result of the expression?';
-const operator = ['+', '-', '*'];
+const operators = ['+', '-', '*'];
 const calculator = new Map([
   ['+', (a, b) => (a + b)],
   ['-', (a, b) => (a - b)],
@@ -11,7 +11,7 @@ const calculator = new Map([
 const getGameData = () => {
   const firstNumber = getRandomInt(1, 100);
   const secondNumber = getRandomInt(1, 100);
-  const operatorRandom = operator[getRandomInt(1, operator.length) - 1];
+  const operatorRandom = operators[getRandomInt(1, operators.length) - 1];
   const calculate = calculator.get(operatorRandom);
   const correctAnswer = calculate(firstNumber, secondNumber);
   const question = `${firstNumber} ${operatorRandom} ${secondNumber}`;
